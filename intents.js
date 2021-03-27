6395e738437d63a33f210e027172b5aef6baaad5
starkah <sarthakgupta.2011@gmail.com>
2021-03-27 16:24:44 +0530

    Updated links

--- intents.js
+++ intents.js
@@ -2,14 +2,15 @@ var list = {
   animals: [
     {
       title: "Vaquite",
-      link: "https://go.echoar.xyz/FC84",
+      link: "https://console.echoar.xyz/webar?key=fragrant-mountain-7260&entry=e16a8d7a-7f4d-4fb8-84ba-925eb9f92cc2",
       left: 10,
       longlat: [26.73, -110.71],
       foundin: ["Northern part of Gulf of Mexico"],
+      
     },
     {
       title: "Asian Elephants",
-      link: "https://go.echoar.xyz/ggvd",
+      link: "https://console.echoar.xyz/webar?key=fragrant-mountain-7260&entry=fa049e65-c187-46e8-b7cd-af8fb34e0b23",
       left: 50000,
       longlat: [8.03, 80.82],
       foundin: [
@@ -20,7 +21,7 @@ var list = {
     },
     {
       title: "Snow Leopard",
-      link: "https://go.echoar.xyz/wb5y",
+      link: "https://console.echoar.xyz/webar?key=fragrant-mountain-7260&entry=0f9ead74-5bc3-4d34-a95d-dcb576128a94",
       left: 6590,
       longlat: [43.66, 101.52],
       foundin: [
@@ -32,7 +33,7 @@ var list = {
     },
     {
       title: "Gorilla",
-      link: "https://go.echoar.xyz/v1Da",
+      link: "https://console.echoar.xyz/webar?key=fragrant-mountain-7260&entry=3adbe5fb-871d-43d2-a514-bf8df391beb7",
       left: 1063,
       longlat: [-1.4, 29.56],
       foundin: [
@@ -44,28 +45,28 @@ var list = {
     },
     {
       title: "Javan Rhinoceros",
-      link: "https://go.echoar.xyz/iHpJ",
+      link: "https://console.echoar.xyz/webar?key=fragrant-mountain-7260&entry=a402342b-0fe2-4c1f-abd5-4109a3c00f7a,
       left: 68,
       longlat: [-6.78, 105.37],
       foundin: ["Ujung Kulon National Park,Indonesia"],
     },
     {
       title: "Irrawaddy Dolphins",
-      link: "https://go.echoar.xyz/DsgN",
+      link: "https://console.echoar.xyz/webar?key=fragrant-mountain-7260&entry=52d52228-4de7-4260-8306-c87a262b234b",
       left: 100,
       longlat: [15.51, 95.14],
       foundin: ["Ayeyarwady River, Myanmar", "Mahakam River, Indonesian"],
     },
     {
       title: "Giant Panda",
-      link: "https://go.echoar.xyz/RsqR",
+      link: "https://console.echoar.xyz/webar?key=fragrant-mountain-7260&entry=60c68fd7-bcd8-40ca-8005-99b82021c9b1",
       left: 1864,
       longlat: [29, 106],
       foundin: ["South-West, China"],
     },
     {
       title: "Tiger",
-      link: "https://go.echoar.xyz/gKne",
+      link: "https://console.echoar.xyz/webar?key=fragrant-mountain-7260&entry=b28a4f8d-599f-4d4d-b22b-bba62c3e45e1",
       left: 3900,
       longlat: [23.41, 80.57],
       foundin: [
@@ -80,7 +81,7 @@ var list = {
     },
     {
       title: "Whooping Crane",
-      link: "https://go.echoar.xyz/4Mph",
+      link: "https://console.echoar.xyz/webar?key=fragrant-mountain-7260&entry=3370f0d9-943f-408a-a9ef-3d0481090b5e",
       left: 505,
       longlat: [39.32, -111.09],
       foundin: ["North-America", "Central Mexico", "Utah"],
@@ -94,8 +95,7 @@ var list = {
     // },
     {
       title: "Blue Whale",
-      link:
-        "https://console.echoar.xyz/webar?key=fragrant-mountain-7260&entry=f2cd0994-f758-45c0-961b-2eefd164413f",
+      link: "https://console.echoar.xyz/webar?key=fragrant-mountain-7260&entry=f2cd0994-f758-45c0-961b-2eefd164413f",
       left: 24000,
       longlat: [36.6, -121.89],
       foundin: [
@@ -108,14 +108,13 @@ var list = {
   plants: [
     {
       title: "Texas Wild rice",
-      link:
-        "https://console.echoar.xyz/webar?key=fragrant-mountain-7260&entry=008f63dd-6726-4f74-b7e4-3b469347e93e",
+      link: "https://console.echoar.xyz/webar?key=fragrant-mountain-7260&entry=008f63dd-6726-4f74-b7e4-3b469347e93e",
       longlat: [29.8, -97.94],
       foundin: ["San Macros"],
     },
     {
       title: "Western Prairie Orchid",
-      link: "https://go.echoar.xyz/gtwc",
+      link: "https://console.echoar.xyz/webar?key=fragrant-mountain-7260&entry=3373896e-8a46-46cd-a50f-aa89640b048c",
       longlat: [53.76, -98.81],
       foundin: [
         "Manitoba",
@@ -130,7 +129,7 @@ var list = {
     },
     {
       title: "Rafflesia",
-      link: "https://go.echoar.xyz/onHi",
+      link: "https://console.echoar.xyz/webar?key=fragrant-mountain-7260&entry=e9ce2e42-d4a7-4a3c-8fa8-7cd33a1c8bef",
       longlat: [0.96, 114.55],
       foundin: ["Borneo", "Sumatra"],
     },
